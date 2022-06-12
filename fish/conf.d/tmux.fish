df272f24f6cb6e270e2802475fe842aae27ab378
dusanveljkovic <dusanveljkovickv@gmail.com>
2022-06-12 13:57:03 +0200

    changed crypto

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -7,7 +7,7 @@ function pen-tmux
   tmux new-session 'cd ~/pentesting_notebooks; vim' \; split-window -h -p 25 'peaclock' \; split-window -v  \; select-pane -t 1 \; 
 end
 
-function cryptopals-tmux 
-  tmux new-session 'cd ~/cryptopals; vim' \; split-window -h -p 30 'ipython' \; split-window -v 'peaclock' \; select-pane -t 1 \;
+function crypto-research-tmux 
+  tmux new-session 'cd ~/crypt-research; vim' \; split-window -h -p 30 'ipython' \; split-window -v 'peaclock' \; select-pane -t 1 \;
 end
 
